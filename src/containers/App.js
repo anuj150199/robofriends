2cf878d5c7ef9234b0180d298565a639212701f6
unknown <anujv1999@yahoo.com>
2020-07-09 13:33:34 +0530

    adding redux

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,16 +1,30 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 import CardList from '../components/CardList';
 import SearchBox from '../components/SearchBox';
 import Scroll from '../components/Scroll';
 import ErrorBoundary from '../components/ErrorBoundary';
 import './App.css';
 
+import {setSearchField} from '../actions'
+
+const mapStateToProps = state => {
+	return {
+		searchField: state.searchField
+		// searchField: state.searchRobots.searchField
+	}
+}
+
+const mapDispatchToProps = (dispatch) => {
+	return {
+		onSearchChange: (event) => dispatch(setSearchField(event.target.value))
+	}
+}
 class App extends Component {
 	constructor(){
 		super()
 		this.state = {
 			robots:[],
-			searchfield:''
 		}
 	}
 	componentDidMount() {
@@ -18,14 +32,12 @@ class App extends Component {
 			.then(response=>response.json())
 			.then(users=>{this.setState({robots:users})});
 	}
-	onSearchChange = (event) => {
-		this.setState({ searchfield: event.target.value})
-	}
 
 	render() {
-		const { robots, searchfield } = this.state;
+		const { robots, } = this.state;
+		const {searchField , onSearchChange } =this.props;
     	const filteredRobots = robots.filter(robot =>{
-      		return robot.name.toLowerCase().includes(searchfield.toLowerCase());
+      		return robot.name.toLowerCase().includes(searchField.toLowerCase());
     })
     if(!robots.length){
     	return <h1>Loading</h1>
@@ -33,7 +45,7 @@ class App extends Component {
 		return(
 			<div className='tc'>
 				<h1 className='f2'> RobotFriends</h1>
-				<SearchBox searchChange={this.onSearchChange}/>
+				<SearchBox searchChange={onSearchChange}/>
 				<Scroll>
 					<ErrorBoundary>
 						<CardList robots ={filteredRobots}/>
@@ -44,4 +56,4 @@ class App extends Component {
 	}
 }
 }
-export default App;
+export default connect(mapStateToProps , mapDispatchToProps)(App);
